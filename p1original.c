1d33df1d8558ea38736f4982b61ed044e1bb7d94
Darshan9347 <94587921+Darshan9347@users.noreply.github.com>
2022-01-22 16:51:43 +0000

    two numbers

---
+++ p1original.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("Enter first number\n");
+  scanf("%d",&a);
+  printf("Enter second number\n");
+  scanf("%d",&b);
+  c=a+b;
+  printf("Sum of two value%d+ %d is %d\n",a,b,c);
+  return 0;
+}
