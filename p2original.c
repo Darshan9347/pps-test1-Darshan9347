9e07932c46169bac7d3ae555dae3e8c25ebbecb8
Darshan9347 <94587921+Darshan9347@users.noreply.github.com>
2022-01-23 03:54:00 +0000

    p2final.c

--- p2original.c
+++ p2original.c
@@ -1,30 +1,31 @@
-#include <stdio.h>
+include<stdio.h>
 int input ()
 {
   int a;
-  printf("Enter Value :");
+  printf("Enter value");
   scanf("%d",&a);
   return a;
 }
+
 int add(int a,int b)
-
 {
-  int sum;
-  sum=a+b;
-  return sum;
+  int c;
+  c=a+b;
+  return c;
 }
-void output(int a,int b,int sum)
 
+int output(int a,int b,int c)
 {
-  printf("sum of two numbers is %d+%d=%d",a,b,sum);
+  printf("sum of two numbers %d+%d+%d",a,b,c);
 }
+
+
 int main()
 {
-  int a,b,result;
+  int a,b,sum;
   a=input();
   b=input();
-  result=add(a,b);
-  output(a,b,result);
+  sum=add(a,b);
+  output(a,b,sum);
   return 0;
-  
-  }
+}
